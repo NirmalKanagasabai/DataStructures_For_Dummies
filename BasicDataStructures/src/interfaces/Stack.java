c8d882b42d8b29fe26901caa63f22b30de71f910
Nirmal Kanagasabai <nirmal.kanagasabai@gmail.com>
2019-02-11 17:32:54 -0500

    Stack Interface added

---
+++ BasicDataStructures/src/interfaces/Stack.java
@@ -0,0 +1,26 @@
+package interfaces;
+
+public interface Stack<X> {
+	
+	/*
+	 *  Push Method to add new items to the stack.
+	 *  @param - Takes in the item that needs to be added to the stack.
+	 */
+	public void push (X item);
+	
+	/*
+	 *  Pop Method to remove items from the top of the stack.
+	 */
+	public X pop ();
+	
+	/*
+	 *  Method to check if the Stack contains the given item.
+	 */
+	public boolean contains (X item);
+	
+	/*
+	 *  Method to access the given item if the Stack has it.
+	 */
+	public X access (X item);
+
+}
