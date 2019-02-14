179419397694a18b9d30153710abffdb10141bba
Nirmal Kanagasabai <nirmal.kanagasabai@gmail.com>
2019-02-13 21:24:29 -0500

    Binary Tree Impl

--- BasicDataStructures/src/implementationClasses/BasicBinaryTreeImpl.java
+++ BasicDataStructures/src/implementationClasses/BasicBinaryTreeImpl.java
@@ -52,6 +52,85 @@ public class BasicBinaryTreeImpl <X extends Comparable<X>> implements BinaryTree
 		}
 	}
 	
+	@Override
+	public boolean contains(X item) {
+		boolean result = false;
+		Node tempNode = getNode(item);
+		
+		if (tempNode == null) {
+			result = false;
+		} else {
+			result = true;
+		}
+		
+		return result;
+	}
+	
+	private Node getNode(X item) {
+		Node tempNode = root;
+		
+		while (tempNode != null) {
+			if (tempNode.getItem().compareTo(item) == 0) {
+				return tempNode;
+			} else if (tempNode.getItem().compareTo(item) < 0) {
+				tempNode = root.getLeft();
+			} else if (tempNode.getItem().compareTo(item) > 0) {
+				tempNode = root.getRight();
+			}
+		}
+		return null;
+	}
+	
+	@Override
+	public boolean delete(X item) {
+		boolean result = false;
+		if(this.root == null) {
+			return false;
+		}
+		
+		Node currentNode = getNode(item);
+		if (currentNode != null) {
+			if (currentNode.getLeft() == null && currentNode.getRight() == null) {
+				unlink(currentNode, null);
+				result = true;
+			} else if (currentNode.getLeft() == null && currentNode.getRight() != null) {
+				unlink(currentNode, currentNode.getRight());
+				result = true;
+			} else if (currentNode.getLeft() == null && currentNode.getRight() != null) {
+				unlink(currentNode, currentNode.getLeft());
+				result = true;
+			} else {
+				Node child = currentNode.getLeft();
+				
+				while (child.getRight() != null && child.getLeft() != null) {
+					child = child.getRight();
+				}
+				
+				child.getParent().setRight(null);
+				child.setLeft(currentNode.getLeft());
+				child.setRight(currentNode.getRight());
+				
+				unlink(currentNode, child);
+				result = true;
+			}
+		} else {
+			result = false;
+		}
+		return result;
+	}
+	
+	private void unlink(Node currentNode, Node newNode) {
+		if (currentNode == this.root) {
+			newNode.setLeft(currentNode.getLeft());
+			newNode.setRight(currentNode.getRight());
+			this.root = newNode;
+		} else if (currentNode.getParent().getRight() == currentNode) {
+			currentNode.getParent().setRight(newNode);
+		} else {
+			currentNode.getParent().setLeft(newNode);
+		}
+	}
+	
 	private class Node {
 		
 		Node left;
--- BasicDataStructures/src/interfaces/BinaryTree.java
+++ BasicDataStructures/src/interfaces/BinaryTree.java
@@ -5,4 +5,8 @@ public interface BinaryTree<X> {
 	public int size();
 	
 	public void add(X item);
+	
+	public boolean contains(X item);
+	
+	public boolean delete (X item);
 }
